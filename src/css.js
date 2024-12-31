3eb56a1d171442ff5be9dcdcbe73b7217bda2952
dbel431 <dbel431@gmail.com>
2024-12-31 11:19:05 +0530

    add z-ind

--- src/css.js
+++ src/css.js
@@ -1,12 +1,12 @@
 export default function css() {
   return `
-    .loader {
+.loader {
   position: fixed;
   top: 0;
   left: 0;
   width: 100%;
   height: 100%;
-
+  z-index:9999;
   display: flex;
   flex-direction: row;
   flex-wrap: nowrap;
